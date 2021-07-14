7fb8f9ee3df11575a8e5361bc52cceb46b5d7332
Andrew King <andrewking1597@gmail.com>
2021-07-14 14:24:13 -0400

    Update SineWave.cpp

--- SineWave.cpp
+++ SineWave.cpp
@@ -1,9 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-
-//todo write to file
-//todo blend sine waves
+#include <string>
+#include <fstream>
 
 class SineWave {
     public:
@@ -15,10 +14,7 @@ class SineWave {
         }
         void print_info();
         int get_array_size();
-
-        //? debugging
-        double get_max();
-
+        void to_wavfile(std::string fname);
     private:
         std::vector<double> data;
         double amplitude;
@@ -29,21 +25,11 @@ class SineWave {
 
         void init_wave();
         void normalize_array(const double max_val, const int max_amplitude=32760);
+        void write_as_bytes(std::ofstream &file, int value, int byte_size);
 };
 
-//? debugging
-double SineWave::get_max() {
-    double maxval = 0;
-    for (int i = 0; i < this->data.size(); i++) {
-        if (this->data[i] > maxval) {
-            maxval = this->data[i];
-        }
-    }
-
-    return maxval;
-}
-
 void SineWave::init_wave() {
+    /* Calculate sine wave values and store in data vector */
 
     // keep track of max value
     double max_value = 0;
@@ -65,6 +51,68 @@ void SineWave::init_wave() {
     // Normalize array
     this->normalize_array(max_value);
 
+    return;
+}
+
+void SineWave::to_wavfile(std::string fname) {
+    /* Write a wav file using the data in data vector */
+    
+    //* Set Header Values
+    // RIFF chunk
+    const std::string chunk_id = "RIFF";
+    int chunk_size; // will be calculated after subchunk2_size
+    const std::string format = "WAVE";
+
+    // FMT sub-chunk
+    const std::string subchunk1_id = "fmt ";
+    const int subchunk1_size = 16; // 16 for PCM
+    const int audio_format = 1; // 1 for PCM
+    const int byte_rate = this->sample_rate * this->num_channels * (subchunk1_size/8);
+    const int block_align = this->num_channels * (subchunk1_size/8);
+    const int bits_per_sample = 16;
+
+    // Data sub-chunk
+    const std::string subchunk2_id = "data";
+    int subchunk2_size = this->data.size() * bits_per_sample / 8;
+    // const int max_amplitude = 32760; // because values are 16-bits
+
+    // calculate chunk_size
+    chunk_size = 36 + subchunk2_size;
+
+    //* Write to File
+    // create ofstream object and open wav file
+    std::ofstream wav;
+    wav.open(fname, std::ios::binary);
+
+    if (wav.is_open()) {
+        // Write RIFF chunk descriptor
+        wav << chunk_id;
+        write_as_bytes(wav, chunk_size, 4);
+        wav << format;
+
+        // Write FMT sub-chunk
+        wav << subchunk1_id;
+        write_as_bytes(wav, subchunk1_size, 4);
+        write_as_bytes(wav, audio_format, 2);
+        write_as_bytes(wav, num_channels, 2);
+        write_as_bytes(wav, sample_rate, 4);
+        write_as_bytes(wav, byte_rate, 4);
+        write_as_bytes(wav, block_align, 2);
+        write_as_bytes(wav, bits_per_sample, 2);
+
+        // Write Data sub-chunk
+        wav << subchunk2_id;
+        write_as_bytes(wav, subchunk2_size, 4);
+
+        // write arr data
+        for (int i = 0; i < this->data.size(); i++) {
+            write_as_bytes(wav, this->data[i], 2);
+        }
+    }
+
+    // Close file
+    wav.close();
+    
     return;
 }
 
@@ -91,4 +139,13 @@ void SineWave::print_info() {
     std::cout << "Max Value =    " << this->get_max() << std::endl;
 
     return;
+}
+
+void SineWave::write_as_bytes(std::ofstream &file, int value, int byte_size) {
+    /*
+    convert numerical values to the correct size (b/c standard sizes vary depending on OS)
+    credit: https://www.youtube.com/watch?v=rHqkeLxAsTc (21:30)
+    */
+
+    file.write(reinterpret_cast<const char*>(&value), byte_size);
 }
